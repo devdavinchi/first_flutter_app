76d2fe829cb046c49ca56ea83d1c971b09da2ebd
dev rai <devdavinchi06@gmail.com>
2025-12-31 23:20:05 +0545

    using exception handling to firebase authorization

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -67,12 +67,17 @@ class _LoginViewState extends State<LoginView> {
                       final email = _email.text;
                       final password = _password.text;
 
-                      final userCredential = await FirebaseAuth.instance
-                          .signInWithEmailAndPassword(
-                            email: email,
-                            password: password,
-                          );
-                      print(userCredential);
+                      try {
+                        final userCredential = await FirebaseAuth.instance
+                            .signInWithEmailAndPassword(
+                              email: email,
+                              password: password,
+                            );
+
+                        print(userCredential);
+                      } catch (e) {
+                        print(e.runtimeType);
+                      }
                     },
                     child: Text("Login"),
                   ),
